d91eabb9c740aa18e8794044274f2b3d65dc6c4d
Joonas Järvinen <j.jarvinen@outlook.com>
2024-11-19 19:20:58 +0200

    Fix linting errors

--- client/e2e-tests/blog_app.spec.js
+++ client/e2e-tests/blog_app.spec.js
@@ -1,65 +1,65 @@
-const { test, expect, beforeEach, describe } = require('@playwright/test')
-const { loginWith, createBlog, getBlogDiv, likeBlog } = require('./helper')
+const { test, expect, beforeEach, describe } = require('@playwright/test');
+const { loginWith, createBlog, getBlogDiv, likeBlog } = require('./helper');
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
-    await request.post('/api/testing/reset')
+    await request.post('/api/testing/reset');
     await request.post('/api/users', {
       data: {
         name: 'Test User',
         username: 'testuser',
         password: 'secret'
       }
-    })
+    });
     await request.post('/api/users', {
       data: {
         name: 'Test User 2',
         username: 'testuser2',
         password: 'secret'
       }
-    })
+    });
 
-    await page.goto('/')
-  })
+    await page.goto('/');
+  });
 
   test('Login form is shown', async ({ page }) => {
-    const locator = page.getByText('log in to application')
-    await expect(locator).toBeVisible()
-    const usernameField = page.getByRole('textbox', { name: 'username' })
-    const passwordField = page.getByRole('textbox', { name: 'password' })
-    await expect(usernameField).toBeVisible()
-    await expect(passwordField).toBeVisible()
-  })
+    const locator = page.getByText('log in to application');
+    await expect(locator).toBeVisible();
+    const usernameField = page.getByRole('textbox', { name: 'username' });
+    const passwordField = page.getByRole('textbox', { name: 'password' });
+    await expect(usernameField).toBeVisible();
+    await expect(passwordField).toBeVisible();
+  });
 
   describe('Login', () => {
     test('succeeds with correct credentials', async ({ page }) => {
-      await loginWith(page, 'testuser', 'secret')
-      const locator = page.getByText('Test User logged in')
-      await expect(locator).toBeVisible()
-    })
+      await loginWith(page, 'testuser', 'secret');
+      const locator = page.getByText('Test User logged in');
+      await expect(locator).toBeVisible();
+    });
 
     test('fails with wrong credentials', async ({ page }) => {
-      await loginWith(page, 'testuser', 'wrong')
-      const locator = page.getByText('invalid username or password')
-      await expect(locator).toBeVisible()
-    })
-  })
+      await loginWith(page, 'testuser', 'wrong');
+      const locator = page.getByText('invalid username or password');
+      await expect(locator).toBeVisible();
+    });
+  });
 
   describe('When logged in', () => {
     beforeEach(async ({ page }) => {
-      await loginWith(page, 'testuser', 'secret')
-    })
-  
+      await loginWith(page, 'testuser', 'secret');
+    });
+
     test('a new blog can be created', async ({ page }) => {
       const newBlog = {
         title: 'Test Blog',
         author: 'Test Author',
         url: 'http://example.com',
       };
-      await createBlog(page, newBlog)
-      const blogList = page.getByTestId('blog-list')
-      await expect(blogList).toContainText(newBlog.title)
-    })
+      await createBlog(page, newBlog);
+      const blogList = page.getByTestId('blog-list');
+      await expect(blogList).toContainText(newBlog.title);
+    });
 
     test('a blog can be liked', async ({ page }) => {
       const newBlog = {
@@ -67,11 +67,11 @@ describe('Blog app', () => {
         author: 'Test Author',
         url: 'http://example.com',
       };
-      await createBlog(page, newBlog)
-      await likeBlog(page, newBlog)
+      await createBlog(page, newBlog);
+      await likeBlog(page, newBlog);
       const blogDiv = await getBlogDiv(page, newBlog);
-      await expect(blogDiv).toContainText('likes 1')
-    })
+      await expect(blogDiv).toContainText('likes 1');
+    });
 
     test('a blog can be deleted', async ({ page }) => {
       const newBlog = {
@@ -81,20 +81,20 @@ describe('Blog app', () => {
       };
 
       page.on('dialog', async dialog => {
-        expect(dialog.message()).toBe(`Remove blog ${newBlog.title} by ${newBlog.author} ?`)
-        await dialog.accept()
+        expect(dialog.message()).toBe(`Remove blog ${newBlog.title} by ${newBlog.author} ?`);
+        await dialog.accept();
       });
 
-      await createBlog(page, newBlog)
+      await createBlog(page, newBlog);
 
       const blogDiv = await getBlogDiv(page, newBlog);
       const viewButton = blogDiv.getByRole('button', { name: 'view' });
-      await viewButton.click()
+      await viewButton.click();
       const removeButton = blogDiv.getByRole('button', { name: 'remove' });
-      await removeButton.click()
+      await removeButton.click();
 
-      await expect(blogDiv).not.toBeVisible()
-    })
+      await expect(blogDiv).not.toBeVisible();
+    });
 
     test('the blog delete button is rendered only for the blog creator', async ({ page }) => {
       const newBlog = {
@@ -102,20 +102,20 @@ describe('Blog app', () => {
         author: 'Test Author',
         url: 'http://example.com',
       };
-      await createBlog(page, newBlog)
+      await createBlog(page, newBlog);
 
       const blogDiv = await getBlogDiv(page, newBlog);
       const viewButton = blogDiv.getByRole('button', { name: 'view' });
-      await viewButton.click()
+      await viewButton.click();
       const removeButton = blogDiv.getByRole('button', { name: 'remove' });
-      await expect(removeButton).toBeVisible()
+      await expect(removeButton).toBeVisible();
 
-      await page.getByRole('button', { name: 'logout' }).click()
-      await loginWith(page, 'testuser2', 'secret')
+      await page.getByRole('button', { name: 'logout' }).click();
+      await loginWith(page, 'testuser2', 'secret');
 
-      await viewButton.click()
-      await expect(removeButton).not.toBeVisible()
-    })
+      await viewButton.click();
+      await expect(removeButton).not.toBeVisible();
+    });
 
     test('blogs are sorted by number of likes', async ({ page }) => {
       const blogs = [
@@ -153,11 +153,11 @@ describe('Blog app', () => {
       });
 
       await likeBlog(page, blogs[2]);
-      await expect(blogDivs[2]).toContainText('likes 1')
+      await expect(blogDivs[2]).toContainText('likes 1');
       await likeBlog(page, blogs[2]);
-      await expect(blogDivs[2]).toContainText('likes 2')
+      await expect(blogDivs[2]).toContainText('likes 2');
       await likeBlog(page, blogs[1]);
-      await expect(blogDivs[1]).toContainText('likes 1')
+      await expect(blogDivs[1]).toContainText('likes 1');
 
       const blogTitlesAfterLikes = await page
         .getByTestId('blog-list')
@@ -166,6 +166,6 @@ describe('Blog app', () => {
       blogTitlesInFinalOrder.forEach((title, index) => {
         expect(blogTitlesAfterLikes[index]).toContain(title);
       });
-    })
-  })
-})
+    });
+  });
+});
